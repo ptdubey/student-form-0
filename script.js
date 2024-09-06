ff39c6d2ef730d4e517859148b7a477bce16daa5
ptdubey <adityababa@gmail.com>
2024-09-06 19:48:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.getElementById('registration-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const password
