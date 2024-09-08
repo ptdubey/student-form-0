ce2fc520273f000fe0106b46d5f7937ab8e54a0a
ptdubey <adityababa@gmail.com>
2024-09-08 22:21:57 +0530

    full changes

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const form = document.getElementById('registration-form');
+let pstate = document.querySelector("#pstate");
+let pcity = document.querySelector("#pcity");
+let pzip = document.querySelector("#pzip");
+let pphonenumber = document.querySelector("#pphonenumber");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const password
+let tstate = document.querySelector("#tstate");
+let tcity = document.querySelector("#tcity");
+let tzip = document.querySelector("#tzip");
+let tphonenumber = document.querySelector("#tphonenumber");
+
+let sameaspermanent = document.querySelector("#sameaspermanent");
+sameaspermanent.addEventListener("change", () => {
+  if (sameaspermanent.checked === true) {
+    tstate.value = pstate.value;
+    tcity.value = pcity.value;
+    tzip.value = pzip.value;
+    tphonenumber.value = pphonenumber.value;
+  } else if (sameaspermanent.checked === false) {
+    tstate.value = "";
+    tcity.value = "";
+    tzip.value = "";
+    tphonenumber.value = "";
+  }
+});
